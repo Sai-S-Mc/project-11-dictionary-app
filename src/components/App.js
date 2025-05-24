58027e6c8efc7dab969c1c66f0522bc9d5b47985
Sai-S-Mc <sai.satti97@gmail.com>
2025-05-24 12:54:51 -0400

    Added basic structure

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,18 @@
 import "../styles/App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
+      <div className="container">
+        <main>
+          <Dictionary />
+        </main>
+        <footer>
+          This dictionary app was designed and coded by Sai Satti, is
+          open-sourced on GitHub and hosted on Netlify
+        </footer>
+      </div>
     </div>
   );
 }
---
+++ src/components/Dictionary.js
@@ -0,0 +1,5 @@
+export default function Dictionary() {
+  return <div className="Dictionary">
+    <h1 className="text-center">Dictionary</h1>
+  </div>;
+}
