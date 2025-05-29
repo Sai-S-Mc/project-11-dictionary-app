05d39f566abf6c722bf02a97b93c60b3b46a88e6
Sai-S-Mc <sai.satti97@gmail.com>
2025-05-29 13:26:31 -0400

    Styled footer

--- src/components/App.js
+++ src/components/App.js
@@ -6,32 +6,34 @@ function App() {
     <div className="App">
       <div className="App-container">
         <Dictionary />
-        <footer>
-          This dictionary app was designed and coded by{" "}
-          <a
-            href="https://github.com/Sai-S-Mc"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Sai Satti
-          </a>{" "}
-          , is{" "}
-          <a
-            href="https://github.com/Sai-S-Mc/project-11-dictionary-app"
-            target="_blank"
-            rel="noreferrer"
-          >
-            open-sourced on GitHub
-          </a>{" "}
-          and hosted on{" "}
-          <a
-            href="https://project-11-dictionary-app.netlify.app/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Netlify
-          </a>
-        </footer>
+        <div className="footer-wrapper">
+          <footer>
+            This dictionary app was designed and coded by{" "}
+            <a
+              href="https://github.com/Sai-S-Mc"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Sai Satti
+            </a>{" "}
+            , is{" "}
+            <a
+              href="https://github.com/Sai-S-Mc/project-11-dictionary-app"
+              target="_blank"
+              rel="noreferrer"
+            >
+              open-sourced on GitHub
+            </a>{" "}
+            and hosted on{" "}
+            <a
+              href="https://project-11-dictionary-app.netlify.app/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Netlify
+            </a>
+          </footer>
+        </div>
       </div>
     </div>
   );
